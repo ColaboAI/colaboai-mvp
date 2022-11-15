a2ffbb1c9a115e5532eb3595725e1085e05ffb09
GoGiants1 <chooi221@snu.ac.kr>
2022-11-15 16:34:36 +0900

    chore: fix login page - vertical align - placeholder to Korean

--- frontend/src/app/containers/SignInPage/index.tsx
+++ frontend/src/app/containers/SignInPage/index.tsx
@@ -42,12 +42,9 @@ export default function SignInPage(props: Props) {
   });
 
   return (
-    <div data-testid="SignInPage">
-      <br></br>
-      <br></br>
-      <br></br>
-      <div className="flex bg-gray-bg1">
-        <div className="w-full max-w-md m-auto bg-white rounded-lg border border-primaryBorder shadow-default py-10 px-16">
+    <div data-testid="SignInPage" className="align-middle h-full">
+      <div className="flex h-full">
+        <div className="w-full max-w-md m-auto bg-white rounded-lg border border-primaryBorder shadow-xl shadow-gray-200  py-10 px-16">
           <h1 className="text-2xl font-medium text-primary mt-4 mb-12 text-center">
             로그인 🔐
           </h1>
@@ -59,7 +56,7 @@ export default function SignInPage(props: Props) {
               className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
               id="email"
               name="email"
-              placeholder="Your Email"
+              placeholder="example@example.com"
               onChange={e => setEmail(e.target.value)}
             />
           </div>
@@ -71,7 +68,7 @@ export default function SignInPage(props: Props) {
               className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
               id="password"
               name="password"
-              placeholder="Your Password"
+              placeholder="비밀번호를 입력하세요."
               onChange={e => setPassword(e.target.value)}
             />
           </div>
